93a115939b0027318be48f2bd8878ce288b81c80
Andrey <andreeey.gg@gmail.com>
2024-10-01 20:30:02 +0200

    add FlagModel

---
+++ app/src/main/java/com/example/currency_converter_android_app/models/FlagModel.java
@@ -0,0 +1,17 @@
+package com.example.currency_converter_android_app.models;
+
+public class FlagModel {
+    private Flags flags;
+
+    public Flags getFlags() {
+        return flags;
+    }
+
+    public static class Flags {
+        private String png;
+
+        public String getPng() {
+            return png;
+        }
+    }
+}
