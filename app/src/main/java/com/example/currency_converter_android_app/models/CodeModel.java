d68dfb818b1ef452c86b5ceeecea404304e9e11c
Andrey <andreeey.gg@gmail.com>
2024-10-02 09:55:57 +0200

    delete comments

--- app/src/main/java/com/example/currency_converter_android_app/models/CodeModel.java
+++ app/src/main/java/com/example/currency_converter_android_app/models/CodeModel.java
@@ -8,22 +8,18 @@ public class CodeModel {
     private String terms_of_use;
     private ArrayList<ArrayList<String>> supported_codes;
 
-    // Getter для result
     public String getResult() {
         return result;
     }
 
-    // Getter для documentation
     public String getDocumentation() {
         return documentation;
     }
 
-    // Getter для terms_of_use
     public String getTermsOfUse() {
         return terms_of_use;
     }
 
-    // Getter для supported_codes
     public ArrayList<ArrayList<String>> getSupportedCodes() {
         return supported_codes;
     }
