264982d63dd7eca4ee66c80e18e61601f0430dfc
Andrey <andreeey.gg@gmail.com>
2024-10-04 10:05:40 +0200

    delete documentation and terms_of_use

--- app/src/main/java/com/example/currency_converter_android_app/models/CodeModel.java
+++ app/src/main/java/com/example/currency_converter_android_app/models/CodeModel.java
@@ -8,25 +8,11 @@ public class CodeModel {
     @SerializedName("result")
     private String result;
 
-    @SerializedName("documentation")
-    private String documentation;
-
-    @SerializedName("terms_of_use")
-    private String termsOfUse;
-
     @SerializedName("supported_codes")
     private ArrayList<ArrayList<String>> supportedCodes;
 
     public String getResult() {
         return result;
-    }
-
-    public String getDocumentation() {
-        return documentation;
-    }
-
-    public String getTermsOfUse() {
-        return termsOfUse;
     }
 
     public ArrayList<ArrayList<String>> getSupportedCodes() {
